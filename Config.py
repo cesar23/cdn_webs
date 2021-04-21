6ff8cb5e6cebd304e2c605f06c29dbaa3a553308
Cesar Auris <perucaos@gmail.com.com>
2021-04-20 20:21:59 -0500

    cmder se actualizo :

--- Config.py
+++ Config.py
@@ -25,7 +25,7 @@ CONFIG = {
     'directorios':["camicv","pcbyte","webcursos","lissacorp","soluciones-tools"], #IMPORTANTE AQUI
     'links':{
         "carpeta -> index":SERVER_PATH_URL+"/index.html"+"?aletorio="+str(tiemp),
-        "carpeta -> soluciones-tools":SERVER_PATH_URL+"/index.html"+"?aletorio="+str(tiemp),
+        "carpeta -> soluciones-tools":SERVER_PATH_URL+"/soluciones-tools/index.html"+"?aletorio="+str(tiemp),
         "carpeta -> camicv":SERVER_PATH_URL+"/camicv/index.html"+"?aletorio="+str(tiemp),
         "carpeta -> pcbyte":SERVER_PATH_URL+"/pcbyte/index.html"+"?aletorio="+str(tiemp),
         "carpeta -> webcursos":SERVER_PATH_URL+"/webcursos/index.html"+"?aletorio="+str(tiemp),
