5b67e2bf65a956f75f577c29a93475e7f0068df3
Cesar Auris <perucaos@gmail.com.com>
2022-06-13 22:00:08 -0500

    cmder se actualizo :

--- Config.py
+++ Config.py
@@ -100,7 +100,7 @@ CONFIG = {
 
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">Drirectorio: {{%CURRENT_DIR%}}</h1>
+                <h1 class="h2">Directorio: {{%CURRENT_DIR%}}</h1>
 
             </div>
 
--- listar_img_webp.py
+++ listar_img_webp.py
@@ -40,6 +40,27 @@ def humanbytes(B):
 plantilla = CONFIG['plantillaGlobal']
 
 
+def get_imagen_details(path_file):
+    """Funcion que devuelve informacion de la imagen de
+
+    Parameters
+    ----------
+    path_file : str
+        parametro path de la imagen
+
+    Returns
+    -------
+    Diccionario
+        devuelve un diccionario
+    """
+    try:
+        image = cv2.imread(path_file)
+        ancho = image.shape[1]  # columnas
+        alto = image.shape[0]  # filas
+        return {"width": ancho, "height": alto}
+    except:
+        return "no  tiene  dimensiones"
+
 def getDimensionImagen(path_file):
     try:
         image = cv2.imread(path_file)
@@ -92,7 +113,9 @@ def generarIndex(path, platilla_html):
             if file.endswith(".webp") or file.endswith(".jpg") or file.endswith(".png") or file.endswith(".svg"):
                 img_list.append(file)
                 img_name = file
-                dimension_imagen = getDimensionImagen(archivo)
+                # dimension_imagen = getDimensionImagen(archivo)
+                imagen_detail = get_imagen_details(archivo)
+
 
                 path_corto = archivo.replace(path, '')
                 # path_corto=NameDir +path_corto
@@ -117,7 +140,8 @@ def generarIndex(path, platilla_html):
                 min_template = """
                    <div class="col-md-3">
                             <div class="card">
-                                <img class="card-img-bottom img-fluid"
+                                <img width="{ancho_imagen}" height="{alto_imagen}" 
+                                class="card-img-bottom img-fluid"
                                     src="https://www.solodev.com/_/images/client-loader.gif" loading="lazy" data-src="{url_imagen}"/>
                                 <div class="card-body">
                                     <h6 class="card-title">{name_imagen}</h6>
@@ -132,7 +156,13 @@ def generarIndex(path, platilla_html):
                             </div>
                         </div>
                 
-                """.format(url_imagen=url_imagen, name_imagen=img_name, peso=peso_archivo, dimensiones=dimension_imagen)
+                """.format(url_imagen=url_imagen,
+                           name_imagen=img_name,
+                           peso=peso_archivo,
+                           dimensiones="ancho:"+imagen_detail['width']+" - "+"alto:"+imagen_detail['height']+"",
+                           ancho_imagen=imagen_detail['width']+"px",
+                           alto_imagen=imagen_detail['height']+"px",
+                           )
 
                 #
                 # str ="https://cesar23.github.io/web_cursos_geral/2020/{}".format(img_name)
