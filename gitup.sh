66a4323356910ef45ae6b83752d518a0bc4198b3
Cesar@PcCasaPrincipal <perucaos@gmail.com>
2024-05-21 10:47:00 -0500

    cmder se actualizo :2024-05-21_10:46:44

--- gitup.sh
+++ gitup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+export LC_ALL="es_ES.UTF-8"
+DATE_HOUR_GIT="`date +%Y`-`date +%m`-`date +%d`_`date +%H`:`date +%M`:`date +%S`"
+CURRENT_USER=$(id -un)
+CURRENT_PC_NAME=$(exec /usr/bin/hostname)
+MY_INFO="${CURRENT_USER}@${CURRENT_PC_NAME}"
+PATH_SCRIPT=`readlink -f "${BASH_SOURCE:-$0}"`
+CURRENT_DIR=`dirname $PATH_SCRIPT`
+NAME_DIR=$(basename $CURRENT_DIR) # nombre del Directorio actual
+
 scriptPathDir=$(dirname $0)
 scriptPathFile=$(realpath $0)
 scriptPathFileName="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -52,3 +61,4 @@ echo "  ██████  ██   ██                ███
 echo ""
 echo ""
 sleep 4
+python -m webbrowser https://cesar23.github.io/cdn_webs/index.html?date=$DATE_HOUR_GIT
